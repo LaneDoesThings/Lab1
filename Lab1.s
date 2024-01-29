63cd025a388ab4010477aead4ef9e44828ac3d99
LaneDoesThings <lane.wright2003@gmail.com>
2024-01-29 15:02:01 -0600

    Added printing arrays

--- Lab1.s
+++ Lab1.s
@@ -19,9 +19,25 @@ main:
     bl printf
 
 
+    bl printArrays
 
+printArrays:
+    push {r0, r1, r2, r3, r10, lr}
+    mov r10, #0 @counter
+    ldr r1, =array1
+    ldr r2, =array2
+    ldr r3, =array3
 
-
+printLoop:
+    ldr r0, =strPrint
+    bl printf
+    ldr r1, [r1, #4] @Next index in array
+    ldr r2, [r2, #4] @Next index in array
+    ldr r3, [r3, #4] @Next index in array
+    
+    cmp r10, #5 @End condition
+    bne printLoop
+    pop {r0, r1, r2, r3, pc}
 
 exit:
     mov r7, #0x01
@@ -35,14 +51,14 @@ strWelcomeMessage: .asciz "Please input 10 numbers (enter a number then hit ente
 .balign 4
 strPrint: .asciz "Array 1: %d, Array 2: %d, Array 3: %d\n"
 
-.balign 16
+.balign 4
 array1: .word 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200
 
 .balign 4
 array2: .word -10, -9, -8, -7, -6, -5, -4, -3, -2, -1
 
 .balign 4
-array3: .word
+array3: .word 0, 0, 0, 0, 0
 
 .balign 4
 intInput: .word 0
